23d6b209519c9fd4f16608285b7148af44cca2ca
Psangwon <pktony@naver.com>
2024-02-19 23:52:22 +0900

    [AnimationClipUtility] refactor
    - Refactored all functions. Now uses EaseType, instead of manual method implementation

--- Assets/Scripts/AnimationClipUtility.cs
+++ Assets/Scripts/AnimationClipUtility.cs
@@ -6,114 +6,24 @@ namespace AnimationClipUtility
 
     public static class AnimationClipUtility
     {
-        #region Linear
-        public static void SetCurveLinearPosition(this AnimationClip clip, Vector2 initialState, Vector2 targetState, float startTime, float duration,
-            string eventFunctionName = "", string destinationHierarchy = "")
-        {
-            clip.SetCurveLinear(AnimationPropertyType.AnchoredPositionX, initialState.x, targetState.x,
-                startTime, duration, eventFunctionName, destinationHierarchy); ;
-            clip.SetCurveLinear(AnimationPropertyType.AnchoredPositionY, initialState.y, targetState.y,
-                startTime, duration, eventFunctionName, destinationHierarchy);
-        }
-
-        public static void SetCurveLinearRotation(this AnimationClip clip, Quaternion initialRotation, Quaternion targetRotation, float startTime, float duration,
-            string eventFunctionName = "", string destinationHierarchy = "")
-        {
-            clip.SetCurveLinear(AnimationPropertyType.RotationX, initialRotation.x, targetRotation.x,
-                startTime, duration, eventFunctionName, destinationHierarchy);
-            clip.SetCurveLinear(AnimationPropertyType.RotationY, initialRotation.y, targetRotation.y,
-                startTime, duration, eventFunctionName, destinationHierarchy);
-            clip.SetCurveLinear(AnimationPropertyType.RotationZ, initialRotation.z, targetRotation.z,
-                startTime, duration, eventFunctionName, destinationHierarchy);
-            clip.SetCurveLinear(AnimationPropertyType.RotationW, initialRotation.w, targetRotation.w,
-                startTime, duration, eventFunctionName, destinationHierarchy);
-        }
-
-        public static void SetCurveLinearSizeDelta(this AnimationClip clip, Vector2 initialSize, Vector2 targetSize, float startTime, float duration,
-            string eventFunctionName = "", string destinationHierarchy = "")
-        {
-            clip.SetCurveLinear(AnimationPropertyType.SizeDeltaX, initialSize.x, targetSize.x,
-                startTime, duration, eventFunctionName, destinationHierarchy);
-            clip.SetCurveLinear(AnimationPropertyType.SizeDeltaY, initialSize.y, targetSize.y,
-                startTime, duration, eventFunctionName, destinationHierarchy);
-        }
-        #endregion
-
-        #region EaseInOut
-        public static void SetCurveEaseInOutPosition(this AnimationClip clip, Vector2 initialState, Vector2 targetState, float startTime, float duration,
-            string eventFunctionName = "", string destinationHierarchy = "")
-        {
-            clip.SetCurveEaseInOut(AnimationPropertyType.AnchoredPositionX, initialState.x, targetState.x,
-                startTime, duration, eventFunctionName, destinationHierarchy);
-            clip.SetCurveEaseInOut(AnimationPropertyType.AnchoredPositionY, initialState.y, targetState.y,
-                startTime, duration, eventFunctionName, destinationHierarchy);
-        }
-
-        public static void SetCurveEaseInOutRotation(this AnimationClip clip, Quaternion initialRotation, Quaternion targetRotation, float startTime, float duration,
-            string eventFunctionName = "", string destinationHierarchy = "")
-        {
-            clip.SetCurveEaseInOut(AnimationPropertyType.RotationX, initialRotation.x, targetRotation.x,
-                startTime, duration, eventFunctionName, destinationHierarchy);
-            clip.SetCurveEaseInOut(AnimationPropertyType.RotationY, initialRotation.y, targetRotation.y,
-                startTime, duration, eventFunctionName, destinationHierarchy);
-            clip.SetCurveEaseInOut(AnimationPropertyType.RotationZ, initialRotation.z, targetRotation.z,
-                startTime, duration, eventFunctionName, destinationHierarchy);
-            clip.SetCurveEaseInOut(AnimationPropertyType.RotationW, initialRotation.w, targetRotation.w,
-                startTime, duration, eventFunctionName, destinationHierarchy);
-        }
-
-        public static void SetCurveEaseIntOutSizeDelta(this AnimationClip clip, Vector2 initialSize, Vector2 targetSize, float startTime, float duration,
-            string eventFunctionName = "", string destinationHierarchy = "")
-        {
-            clip.SetCurveEaseInOut(AnimationPropertyType.SizeDeltaX, initialSize.x, targetSize.x,
-                startTime, duration, eventFunctionName, destinationHierarchy);
-            clip.SetCurveEaseInOut(AnimationPropertyType.SizeDeltaY, initialSize.y, targetSize.y,
-                startTime, duration, eventFunctionName, destinationHierarchy);
-        }
-        #endregion
-
-        #region EaseOut
-
-        #endregion
-
-        #region Constant
-        public static void SetCurveConstantPosition(this AnimationClip clip, Vector2 targetState, float duration,
-            string eventFunctionName = "", string destinationHierarchy = "")
-        {
-            clip.SetCurveConstant(AnimationPropertyType.AnchoredPositionX, targetState.x,
-                duration, eventFunctionName, destinationHierarchy);
-            clip.SetCurveConstant(AnimationPropertyType.AnchoredPositionY, targetState.y,
-                duration, eventFunctionName, destinationHierarchy);
-        }
-
-        public static void SetCurveConstantRotation(this AnimationClip clip, Quaternion targetRotation, float duration,
-            string eventFunctionName = "", string destinationHierarchy = "")
-        {
-            clip.SetCurveConstant(AnimationPropertyType.RotationX, targetRotation.x,
-                duration, eventFunctionName, destinationHierarchy);
-            clip.SetCurveConstant(AnimationPropertyType.RotationY, targetRotation.y,
-                duration, eventFunctionName, destinationHierarchy);
-            clip.SetCurveConstant(AnimationPropertyType.RotationZ, targetRotation.z,
-                duration, eventFunctionName, destinationHierarchy);
-            clip.SetCurveConstant(AnimationPropertyType.RotationW, targetRotation.w,
-                duration, eventFunctionName, destinationHierarchy);
-        }
-
-        public static void SetCurveConstantSizeDelta(this AnimationClip clip, Vector2 targetSize, float duration,
-            string eventFunctionName = "", string destinationHierarchy = "")
-        {
-            clip.SetCurveConstant(AnimationPropertyType.SizeDeltaX, targetSize.x,
-                duration, eventFunctionName, destinationHierarchy);
-            clip.SetCurveConstant(AnimationPropertyType.SizeDeltaY, targetSize.y,
-                duration, eventFunctionName, destinationHierarchy);
-        }
-        #endregion
-
         #region Base
-
+        /// <summary>
+        /// Sets an animation curve to the animation clip
+        /// </summary>
+        /// <param name="clip"></param>
+        /// <param name="animationPropertyType"></param>
+        /// <param name="easeType"></param>
+        /// <param name="startPos"></param>
+        /// <param name="endPos"></param>
+        /// <param name="startTime"></param>
+        /// <param name="duration">duration(end time) of the animation</param>
+        /// <param name="eventFunctionName">name of the custom event triggered in the animation. 
+        /// The event is triggered at the end of the animation. Use <see cref="AddEvent"/> if you want to trigger at a speicific time.</param>
+        /// <param name="destinationHierarchy">A specific hierarchy the object is animated. Use '/'to separate each hierachy</param>
+        /// <returns></returns>
         public static AnimationCurve SetCurve(this AnimationClip clip, AnimationPropertyType animationPropertyType,
             EaseType easeType,
-            float initialPosition, float endPosition, float startTime, float duration,
+            float startPos, float endPos, float startTime, float duration,
             string eventFunctionName = "", string destinationHierarchy = "")
         {
             if (AnimationPropertyType.CombinedType.Contains(animationPropertyType))
@@ -122,7 +32,7 @@ namespace AnimationClipUtility
                 return null ;
             }
 
-            var curve = EasingCore.GetCurve(easeType, startTime, duration, initialPosition, endPosition);
+            var curve = EasingCore.GetCurve(easeType, startTime, duration, startPos, endPos);
 
             clip.SetCurve(destinationHierarchy, AnimationPropertyType.PropertyType[animationPropertyType], animationPropertyType.Name, curve);
 
@@ -131,133 +41,22 @@ namespace AnimationClipUtility
 
             return curve;
         }
-        /// <summary>
-        /// 동적으로 애니메이션 클립 키프레임을 삽입해야 하는 경우 사용하는 함수 (EASE-IN-OUT)
-        /// </summary>
-        /// <param name="animationPropertyType">애니메이션의 움직임 종류</param>
-        /// <param name="initialPosition">시작 위치</param>
-        /// <param name="endPosition">끝 위치 </param>
-        /// <param name="duration">지속 시간 (second)</param>
-        /// <param name="clip">애니메이션 클립</param>
-        /// <param name="eventFunctionName">이벤트 함수 이름</br>이벤트 이름은</param>
-        /// <param name="destinationHierarchy">키 프레임이 삽입될 하이라키 상 경로</param>
-        public static void SetCurveEaseInOut(this AnimationClip clip, AnimationPropertyType animationPropertyType,
-            float initialPosition, float endPosition, float startTime, float duration,
-            string eventFunctionName = "", string destinationHierarchy = "")
-        {
-            if (AnimationPropertyType.CombinedType.Contains(animationPropertyType))
-            {
-                Debug.LogWarning($"AnimationClipUtility : Should not use combined type for this function. use combined function instead");
-                return;
-            }
-
-            Keyframe[] keyframes = new Keyframe[2];
-            keyframes[0] = new Keyframe(startTime, initialPosition);
-            keyframes[1] = new Keyframe(duration, endPosition);
-            AnimationCurve curve = new(keyframes);
-
-            clip.SetCurve(destinationHierarchy, AnimationPropertyType.PropertyType[animationPropertyType], animationPropertyType.Name, curve);
-
-            if (string.IsNullOrEmpty(eventFunctionName)) return;
-            clip.AddEvent(duration, eventFunctionName);
-        }
 
         /// <summary>
-        /// 동적으로 애니메이션 클립 키프레임을 삽입해야 하는 경우 사용하는 함수 (EASE-OUT)
+        /// Resets all the events in an animation clip.
         /// </summary>
-        /// <param name="animationPropertyType">애니메이션의 움직임 종류</param>
-        /// <param name="initialPosition">시작 위치</param>
-        /// <param name="endPosition">끝 위치 </param>
-        /// <param name="duration">지속 시간 (second)</param>
-        /// <param name="clip">애니메이션 클립</param>
-        /// <param name="eventFunctionName">이벤트 함수 이름</br>이벤트 이름은</param>
-        /// <param name="destinationHierarchy">키 프레임이 삽입될 하이라키 상 경로</param>
-        public static void SetCurveEaseOut(this AnimationClip clip, AnimationPropertyType animationPropertyType,
-            float initialPosition, float endPosition, float startTime, float duration,
-            string eventFunctionName = "", string destinationHierarchy = "")
-        {
-            if (AnimationPropertyType.CombinedType.Contains(animationPropertyType))
-            {
-                Debug.LogWarning($"AnimationClipUtility : Should not use combined type for this function. use combined function instead");
-                return;
-            }
-
-            float slope = (endPosition - initialPosition) / (duration - startTime);
-            Keyframe[] array = new Keyframe[2]
-            {
-                new Keyframe(startTime, initialPosition, 0f, slope),
-                new Keyframe(duration, endPosition, 0f, 0f)
-            };
-            var curve = new AnimationCurve(array);
-
-            clip.SetCurve(destinationHierarchy, AnimationPropertyType.PropertyType[animationPropertyType], animationPropertyType.Name, curve);
-
-            if (string.IsNullOrEmpty(eventFunctionName)) return;
-            clip.AddEvent(duration, eventFunctionName);
-        }
-
-        /// <summary>
-        /// 동적으로 애니메이션 클립 키프레임을 삽입해야 하는 경우 사용하는 함수 (Linear)
-        /// </summary>
-        /// <param name="animationPropertyType">애니메이션의 움직임 종류</param>
-        /// <param name="initialPosition">시작 위치</param>
-        /// <param name="endPosition">끝 위치 </param>
-        /// <param name="duration">지속 시간 (second)</param>
-        /// <param name="clip">애니메이션 클립</param>
-        /// <param name="eventFunctionName">이벤트 함수 이름</param>
-        /// <param name="destinationHierarchy">키 프레임이 삽입될 하이라키 상 경로</param>
-        public static void SetCurveLinear(this AnimationClip clip, AnimationPropertyType animationPropertyType,
-            float initialPosition, float endPosition, float startTime, float duration,
-            string eventFunctionName = "", string destinationHierarchy = "")
-        {
-            if (AnimationPropertyType.CombinedType.Contains(animationPropertyType))
-            {
-                Debug.LogWarning($"AnimationClipUtility : Should not use combined type for this function. Use combined function instead");
-                return;
-            }
-
-            AnimationCurve curve = AnimationCurve.Linear(startTime, initialPosition, duration, endPosition);
-
-            clip.SetCurve(destinationHierarchy, AnimationPropertyType.PropertyType[animationPropertyType], animationPropertyType.Name, curve);
-
-            if (string.IsNullOrEmpty(eventFunctionName)) return;
-            clip.AddEvent(duration, eventFunctionName);
-        }
-
-        /// <summary>
-        /// 동적으로 애니메이션 클립 키프레임을 삽입해야 하는 경우 사용하는 함수 (Constant)
-        /// </summary>
-        /// <param name="animationPropertyType">애니메이션의 움직임 종류</param>
-        /// <param name="initialPosition">시작 위치</param>
-        /// <param name="endPosition">끝 위치 </param>
-        /// <param name="duration">지속 시간 (second)</param>
-        /// <param name="clip">애니메이션 클립</param>
-        /// <param name="eventFunctionName">이벤트 함수 이름</param>
-        /// <param name="destinationHierarchy">키 프레임이 삽입될 하이라키 상 경로</param>
-        public static void SetCurveConstant(this AnimationClip clip, AnimationPropertyType animationPropertyType,
-            float endPosition, float duration,
-            string eventFunctionName = "", string destinationHierarchy = "")
-        {
-            if (AnimationPropertyType.CombinedType.Contains(animationPropertyType))
-            {
-                Debug.LogWarning($"AnimationClipUtility : Should not use combined type for this function. Use combined function instead");
-                return;
-            }
-
-            AnimationCurve curve = AnimationCurve.Constant(0f, duration, endPosition);
-
-            clip.SetCurve(destinationHierarchy, AnimationPropertyType.PropertyType[animationPropertyType], animationPropertyType.Name, curve);
-
-            if (string.IsNullOrEmpty(eventFunctionName)) return;
-            clip.AddEvent(duration, eventFunctionName);
-        }
-
+        /// <param name="clip"></param>
         public static void ResetAllEvent(this AnimationClip clip)
         {
             clip.events = null;
         }
 
-
+        /// <summary>
+        /// Adds an event to an animation clip
+        /// </summary>
+        /// <param name="clip"></param>
+        /// <param name="eventTime"></param>
+        /// <param name="eventFunctionName"></param>
         public static void AddEvent(this AnimationClip clip, float eventTime, string eventFunctionName)
         {
             AnimationEvent animEvent = new();
@@ -266,5 +65,39 @@ namespace AnimationClipUtility
             clip.AddEvent(animEvent);
         }
         #endregion
+    
+        public static void SetCurvePosition(this AnimationClip clip, EaseType easeType,
+            Vector2 initialPos, Vector2 targetPos, float startTime, float duration,
+            string eventFunctionName = "", string destinationHierarchy = "")
+        {
+            clip.SetCurve(AnimationPropertyType.AnchoredPositionX, easeType, initialPos.x, targetPos.x,
+                startTime, duration, eventFunctionName, destinationHierarchy);
+            clip.SetCurve(AnimationPropertyType.AnchoredPositionY, easeType, initialPos.y, targetPos.y,
+                startTime, duration, eventFunctionName, destinationHierarchy);
+        }
+
+        public static void SetCurveRotation(this AnimationClip clip, EaseType easeType,
+            Quaternion initialRotation, Quaternion targetRotation, float startTime, float duration,
+            string eventFunctionName = "", string destinationHierarchy = "")
+        {
+            clip.SetCurve(AnimationPropertyType.RotationX, easeType, initialRotation.x, targetRotation.x,
+                startTime, duration, eventFunctionName, destinationHierarchy);
+            clip.SetCurve(AnimationPropertyType.RotationY, easeType, initialRotation.y, targetRotation.y,
+                startTime, duration, eventFunctionName, destinationHierarchy);
+            clip.SetCurve(AnimationPropertyType.RotationZ, easeType, initialRotation.z, targetRotation.z,
+                startTime, duration, eventFunctionName, destinationHierarchy);
+            clip.SetCurve(AnimationPropertyType.RotationW, easeType, initialRotation.w, targetRotation.w,
+                startTime, duration, eventFunctionName, destinationHierarchy);
+        }
+
+        public static void SetCurveSizeDelta(this AnimationClip clip, EaseType easeType,
+            Vector2 initialSize, Vector2 targetSize, float startTime, float duration,
+            string eventFunctionName = "", string destinationHierarchy = "")
+        {
+            clip.SetCurve(AnimationPropertyType.SizeDeltaX, easeType, initialSize.x, targetSize.x,
+                startTime, duration, eventFunctionName, destinationHierarchy);
+            clip.SetCurve(AnimationPropertyType.SizeDeltaY, easeType, initialSize.y, targetSize.y,
+                startTime, duration, eventFunctionName, destinationHierarchy);
+        }
     }
 }
---
+++ README.md
@@ -0,0 +1,139 @@
+# Animation Utility For Unity
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Animation Curve](#example)
+- [Code Overview](#code_overview)
+- [Reference](#references)
+
+## About <a name = "about"></a>
+
+This repository implements animation utility for Unity 3D. It uses legacy animation clip and animation curve. It is implemented base on the official Unity document + a. see [referece](#references) section for details.
+
+I'm not an expert at animation, so I had to make an easy way to animate UIs. I understand that the function has multiple parameters, and not easy to understand at a glance, nevertheless, tried my best to generalize and simplify the function. Feel free to modify this code! Use your imagination to develop this codes !
+
+Also take a look at my blog for more detailed explanation. (KOREAN) [Tistory Blog](coming soon)
+
+**NOTICE** <br>
+Some animation curves are not really based on the exact equation. But I tried my best to look similar. see [reference](#references) where I referenced curves.
+
+## Getting Started <a name = "getting_started"></a>
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See [deployment](#deployment) for notes on how to deploy the project on a live system.
+
+### Tested On
+
+Unity 2021.3.33f1
+Currently live on a product. (AOS / iOS)
+
+### Installation
+
+A step by step series of examples that tell you how to get a development env running.
+
+```
+Unity Package git url
+```
+
+Now you are all set ! Enjoy animating !
+
+## Example <a name = "example"></a>
+
+Refer to Example Scene in the package.
+
+## Code Overview <a name= "code_overview"></a>
+
+### AnimationClipUtility
+An extension class for animation. Mostly extends Animation Clip.
+#### SetCurve
+```
+public static AnimationCurve SetCurve(this AnimationClip clip, 
+    AnimationPropertyType animationPropertyType,
+    EaseType easeType,
+    float startPos, float endPos, float startTime, float duration,
+    string eventFunctionName = "", string destinationHierarchy = "")
+```
+##### Details
+Extends AnimationClip. Base function of all the other methods. 
+##### Parameters
+[AnimationPropertyType](#animation_property_type) Type of the property you would like to animate<br>
+[EaseType](#ease_type) Type of curve<br>
+startPos(float) | initial value of the animation<br>
+endPos(float) | final value of the animation<br>
+startTime(float) | initial time of the animation (usually 0).<br>
+duration(float) | duration of the animation.
+eventFunctionName(string) | name of the custom function, you would like to trigger. Leave empty if no event is triggered.<br>
+destinationHierarachy(string) | hierarchy of the object you want to animate. Leave empty if animating the object where animation component is attached.<br>
+
+#### SetCurvePosition
+```
+public static void SetCurvePosition(this AnimationClip clip, EaseType easeType,
+        Vector2 initialPos, Vector2 targetPos, float startTime, float duration,
+        string eventFunctionName = "", string destinationHierarchy = "")
+```
+##### Details
+Sets a **position** animation curve to the clip.
+
+#### SetCurveRotation
+```
+public static void SetCurveRotation(this AnimationClip clip, EaseType easeType,
+        Quaternion initialRotation, Quaternion targetRotation, float startTime, float duration,
+        string eventFunctionName = "", string destinationHierarchy = "")
+```
+##### Details
+Sets a **rotation** animation curve to the clip.
+
+#### SetCurveSizeDelta
+```
+public static void SetCurveSizeDelta(this AnimationClip clip, EaseType easeType,
+        Vector2 initialSize, Vector2 targetSize, float startTime, float duration,
+        string eventFunctionName = "", string destinationHierarchy = "")
+```
+##### Details
+Sets a **Size Delta** animation curve to the clip.
+
+--- 
+
+### AnimationPropertyType <a name="animation_property_type"></a>
+Defines the name of the animation property. i.e.) anchored position => m_AnchoredPosition
+#### Supported Types
+```
+AnchoredPostion (X, Y)
+Rotation (X, Y, Z)
+SizeDelta (X, Y)
+Scale (X, Y, Z)
+
+TMPRO Font Size
+Sprite
+Alpha
+```
+
+You can actually add more properties yourself as long as you know the Unity's internal name for each properties.
+
+---
+
+### EasingCore <a name="easing_core"></a>
+#### EaseType
+```
+Linear,
+EaseInOut,
+EaseOut,
+EaseIn,
+Constant,
+ExponentialOut,
+ExponentialIn,
+ExponentialInOut,
+EaseInBack,
+EaseOutBack,
+EaseInOutBack,
+BounceOut,
+BounceIn,
+```
+
+## Reference <a name = "references"></a>
+- [Figma Learn - Prototype easing and spring animations](#https://help.figma.com/hc/en-us/articles/360051748654-Prototype-easing-and-spring-animations#Easing_Bezier_presets)
+- [Unity-EasingLibraryVisualization](#https://github.com/noisecrime/Unity-EasingLibraryVisualisation)
+- [Unity - AnimationCurve](#https://docs.unity3d.com/ScriptReference/AnimationCurve-ctor.html)
+- [Unity - AnimationClip](#https://docs.unity3d.com/ScriptReference/AnimationClip.html)
+- [Unity - Keyframe](#https://docs.unity3d.com/ScriptReference/Keyframe.html)
